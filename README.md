46492271e81fd40333ebe3beb229294455ad5d8c
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-05-12 07:19:02 +0200

    chore: cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # action-publish-vscode-extension
+
 An action to publish Visual Studio Code extensions
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,46 @@
-import { existsSync } from 'node:fs'
-import { getBooleanInput, getInput, setFailed, setOutput } from '@actions/core'
-import {
-  publish
-} from "ovsx";
+import process from 'node:process'
+import { getBooleanInput, getInput, setFailed } from '@actions/core'
 
-import {
-  publish as publishVSCE,
-} from "@vscode/vsce";
+// const REGISTRIES = {
+//   VSCE: 'https://marketplace.visualstudio.com',
+//   OVSX: 'https://open-vsx.org',
+// } as const
 
-const REGISTRIES = {
-  VSCE: 'https://marketplace.visualstudio.com',
-  OVSX: 'https://open-vsx.org',
-} as const
+// type Registry = keyof typeof REGISTRIES
 
-type Registry = keyof typeof REGISTRIES
+// async function getExtensionFile(_extensionFile: string) {
 
-async function getExtensionFile(extensionFile: string) {
-
-}
+// }
 
 async function run() {
-  const token = getInput('token', {
+  const _token = getInput('token', {
     required: true,
   })
-  const registry = getInput('registry')
-  let extensionPath = getInput('extensionPath')
-  const listFiles = getBooleanInput('debugVSIXFile')
-  const publish = getBooleanInput('publish')
-  const preRelease = getBooleanInput('preRelease')
+  const _registry = getInput('registry')
+  const _extensionPath = getInput('extensionPath')
+  const _listFiles = getBooleanInput('debugVSIXFile')
+  const _publish = getBooleanInput('publish')
+  const _preRelease = getBooleanInput('preRelease')
 
-  if (!Object.keys(REGISTRIES).includes(registry.toUpperCase())) {
-    setFailed(`Invalid registry, must be one of: ${Object.keys(REGISTRIES).join(', ')}`)
-  }
+  // if (!Object.keys(REGISTRIES).includes(registry.toUpperCase())) {
+  //   setFailed(`Invalid registry, must be one of: ${Object.keys(REGISTRIES).join(', ')}`)
+  // }
 
-  const registryUrl = REGISTRIES[registry.toUpperCase() as Registry]
-  setOutput('registryUrl', registryUrl)
-  setOutput('registry', registry)
+  // const registryUrl = REGISTRIES[registry.toUpperCase() as Registry]
+  // setOutput('registryUrl', registryUrl)
+  // setOutput('registry', registry)
 
-  if (!extensionPath) {
-    if (!existsSync(extensionPath)) {
-      setFailed(`File not found: ${extensionPath}`)
-    }
-    extensionPath = await getExtensionFile(extensionPath)
-    setOutput('extensionFile', extensionPath)
-  }
+  // if (!extensionPath) {
+  //   if (!existsSync(extensionPath)) {
+  //     setFailed(`File not found: ${extensionPath}`)
+  //   }
+  //   extensionPath = await getExtensionFile(extensionPath)
+  //   setOutput('extensionFile', extensionPath)
+  // }
 
-  if (publish) {
-    console.log('Publishing extension')
-  }
+  // if (publish) {
+  //   console.log('Publishing extension')
+  // }
 }
 
 run().catch((err) => {
