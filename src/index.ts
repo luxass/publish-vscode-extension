ec0d84ddbd4d73e308bd9030aeabd281b392e330
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-12 10:51:51 +0200

    dump

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import process from "node:process";
 import * as core from "@actions/core";
 
 async function run() {
-  const _token = core.getInput("token", {
-    required: true,
-  });
-  const _registry = core.getInput("registry");
-  const _extensionPath = core.getInput("extensionPath");
-  const _listFiles = core.getBooleanInput("debugVSIXFile");
-  const _publish = core.getBooleanInput("publish");
-  const _preRelease = core.getBooleanInput("preRelease");
+	const _token = core.getInput("token", {
+		required: true,
+	});
+	const _registry = core.getInput("registry");
+	const _extensionPath = core.getInput("extensionPath");
+	const _listFiles = core.getBooleanInput("debugVSIXFile");
+	const _publish = core.getBooleanInput("publish");
+	const _preRelease = core.getBooleanInput("preRelease");
 
-  core.info("Hello, World!");
+	core.info("Hello, World!");
 }
 
 run().catch((err) => {
-  console.error(err);
-  core.setFailed(err);
-  process.exit(1);
+	console.error(err);
+	core.setFailed(err);
+	process.exit(1);
 });
