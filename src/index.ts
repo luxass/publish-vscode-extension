11cfaf1ccd5820090883ac7c63531abd0fdae467
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-12 15:22:54 +0200

    feat: publish extension

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import * as core from "@actions/core";
 import { REGISTRIES } from "./constants";
 import * as fs from "node:fs";
 import { createVsix } from "./vsix";
+import { publishVSIX as publishVSCE } from "@vscode/vsce";
+import { publish as publishOVSX } from "ovsx"
 
 async function run() {
-	const _token = core.getInput("token", {
+	const token = core.getInput("token", {
 		required: true,
 	});
 	const registry = core.getInput("registry", {
@@ -45,16 +47,37 @@ async function run() {
 		return;
 	}
 
+	let extensionFile: string;
 	if (!isFile) {
 		const result = await createVsix({ dir: extensionPath, outfile: "extension.vsix", dry: dryRun });
 
 		core.info(`created vsix at: ${JSON.stringify(result, null, 2)}`);
 
+		extensionFile = result.outfile
 	} else {
+		extensionFile = extensionPath;
 		core.info("extension is already packaged, skipping.")
 	}
 
+
 	const preRelease = core.getBooleanInput("pre-release");
+
+	if (dryRun) {
+		core.info("dry-run enabled, skipping publish");
+		return;
+	}
+
+	if (registry === "vs-marketplace") {
+		const result = await publishVSCE(extensionFile, {
+			pat: token,
+			preRelease,
+		});
+
+		core.info(`published to marketplace: ${JSON.stringify(result, null, 2)}`);
+	} else {
+		throw new Error("registry not supported");
+	}
+
 }
 
 run().catch((err) => {
