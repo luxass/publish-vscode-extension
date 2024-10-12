b6982eb4360abebd35b74b97fa57bdefa925256a
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-12 11:34:51 +0200

    feat: implement some extensionPath detection

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const REGISTRIES = {
+	"open-vsx": "https://open-vsx.org",
+	"vs-marketplace": "https://marketplace.visualstudio.com",
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,66 @@
 import * as core from "@actions/core";
+import { REGISTRIES } from "./constants";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 async function run() {
 	const _token = core.getInput("token", {
 		required: true,
 	});
-	const registry = core.getInput("registry");
-	const extensionPath = core.getInput("extensionPath");
+	const registry = core.getInput("registry", {
+		trimWhitespace: true,
+	});
 	const debug = core.getBooleanInput("debug");
-	const publish = core.getBooleanInput("publish");
-	const preRelease = core.getBooleanInput("preRelease");
 	const dryRun = core.getBooleanInput("dry-run");
 
+	if (debug) {
+		core.warning("running with debug mode enabled");
+	}
+
+	if (dryRun) {
+		core.warning("running with dry-run mode enabled");
+	}
+
+	if (!URL.canParse(registry) && !Object.keys(REGISTRIES).includes(registry)) {
+		core.setFailed(`invalid registry used: ${registry}`);
+		return;
+	}
+
+	const extensionPath = core.getInput("extensionPath", {
+		trimWhitespace: true,
+	});
+
+	core.debug(`extensionPath: ${extensionPath}`);
+
+	if (!fs.existsSync(extensionPath)) {
+		core.setFailed("extensionPath is not a valid path");
+		return;
+	}
+
+	const isFile = fs.lstatSync(extensionPath).isFile();
+	const isDirectory = fs.lstatSync(extensionPath).isDirectory();
+
+	if (!isFile && !isDirectory) {
+		core.setFailed("extensionPath is not a valid path");
+		return;
+	}
+
+	core.info(`extensionPath is a ${isFile ? "file" : "directory"}`);
+
+	const publish = core.getBooleanInput("publish");
+	const preRelease = core.getBooleanInput("preRelease");
+
 	core.info("Hello, World!");
-	core.info(JSON.stringify({
-		registry,
-		extensionPath,
-		debug,
-		publish,
-		preRelease,
-		dryRun
-	}))
+	core.info(
+		JSON.stringify({
+			registry,
+			extensionPath,
+			debug,
+			publish,
+			preRelease,
+			dryRun,
+		}),
+	);
 }
 
 run().catch((err) => {
