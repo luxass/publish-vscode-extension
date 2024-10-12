6b213f91c23d27be3f32a2021df449df3768dea9
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-12 10:44:07 +0200

    dump

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,21 @@
 import process from "node:process";
-import { getBooleanInput, getInput, setFailed } from "@actions/core";
-
-// const REGISTRIES = {
-//   VSCE: 'https://marketplace.visualstudio.com',
-//   OVSX: 'https://open-vsx.org',
-// } as const
-
-// type Registry = keyof typeof REGISTRIES
-
-// async function getExtensionFile(_extensionFile: string) {
-
-// }
+import * as core from "@actions/core";
 
 async function run() {
-  const _token = getInput("token", {
+  const _token = core.getInput("token", {
     required: true,
   });
-  const _registry = getInput("registry");
-  const _extensionPath = getInput("extensionPath");
-  const _listFiles = getBooleanInput("debugVSIXFile");
-  const _publish = getBooleanInput("publish");
-  const _preRelease = getBooleanInput("preRelease");
+  const _registry = core.getInput("registry");
+  const _extensionPath = core.getInput("extensionPath");
+  const _listFiles = core.getBooleanInput("debugVSIXFile");
+  const _publish = core.getBooleanInput("publish");
+  const _preRelease = core.getBooleanInput("preRelease");
 
-  // if (!Object.keys(REGISTRIES).includes(registry.toUpperCase())) {
-  //   setFailed(`Invalid registry, must be one of: ${Object.keys(REGISTRIES).join(', ')}`)
-  // }
-
-  // const registryUrl = REGISTRIES[registry.toUpperCase() as Registry]
-  // setOutput('registryUrl', registryUrl)
-  // setOutput('registry', registry)
-
-  // if (!extensionPath) {
-  //   if (!existsSync(extensionPath)) {
-  //     setFailed(`File not found: ${extensionPath}`)
-  //   }
-  //   extensionPath = await getExtensionFile(extensionPath)
-  //   setOutput('extensionFile', extensionPath)
-  // }
-
-  // if (publish) {
-  //   console.log('Publishing extension')
-  // }
+  core.info("Hello, World!");
 }
 
 run().catch((err) => {
   console.error(err);
-  setFailed(err);
+  core.setFailed(err);
   process.exit(1);
 });
