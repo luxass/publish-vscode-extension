87afff4562956721f5a0ac8d2b5cd6e9a98b5c64
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-27 15:00:06 +0100

    refactor: use actions-kit

---
+++ actions-kit.config.ts
@@ -0,0 +1,89 @@
+import { defineConfig } from "actions-kit/config";
+import rspack from "@actions-sdk/rspack-builder";
+
+export default defineConfig({
+	writeYaml: true,
+	action: {
+		name: "Publish Visual Studio Code Extension",
+		description:
+			"An action to publish Visual Studio Code Extension to Open VSX Registry or Visual Studio Marketplace",
+		author: "luxass",
+		branding: {
+			color: "purple",
+			icon: "package",
+		},
+		inputs: {
+			token: {
+				description: "The token to use for authentication.",
+				required: true,
+			},
+			registry: {
+				description:
+					"The registry to publish to. (URL's are supported, but also the values 'open-vsx' and 'vs-marketplace' are supported.)",
+				required: true,
+				default: "vs-marketplace",
+			},
+			"extension-path": {
+				description: "Path to VSIX File or path the extension you want to publish.",
+				required: false,
+				default: "./",
+			},
+			debug: {
+				description:
+					"Show debug information. (This is not the same as enabling debug logging for the workflow rerun)",
+				required: false,
+				default: "false",
+			},
+			"dry-run": {
+				description:
+					"Do not publish the extension, but show the information that would be sent to the registry.",
+				required: false,
+				default: "false",
+			},
+			"pre-release": {
+				description: "Mark the extension as pre-release extension.",
+				required: false,
+				default: "false",
+			},
+			manager: {
+				description:
+					"The package manager to use. (we automatically detect the package manager if you don't specify it)",
+				required: false,
+			},
+			"fail-silently": {
+				description: "Do not fail if the extension is already published.",
+				required: false,
+				default: "false",
+			},
+			targets: {
+				description: "Target architecture(s) the extension should run on. (Comma separated list)",
+				required: false,
+			},
+			"base-content-url": {
+				description: "Prepend all relative links in README.md with this URL.",
+				required: false,
+			},
+			"base-images-url": {
+				description: "Prepend all relative image links in README.md with this URL.",
+				required: false,
+			},
+		},
+		outputs: {
+			vsix: {
+				description: "The path to the VSIX file.",
+			},
+			success: {
+				description: "Whether the action was successful.",
+			},
+		},
+		runs: {
+			using: "node20",
+			main: "dist/index.cjs",
+		},
+	},
+	builder: rspack({
+    externals: {
+      keytar: "commonjs keytar",
+    },
+  }),
+});
